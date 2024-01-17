a81556c9e16d8933b8ecce145ae52d13091f0918
Guilhermee19 <guilherme@noclaftech.com>
2024-01-16 23:07:13 -0300

    feat: tela e scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'age-calculator-app';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private fb: FormBuilder,
+  ){}
+
+  age_form = this.fb.group({
+    day: ['', Validators.required],
+    month: ['', Validators.required],
+    year: ['', Validators.required],
+  })
+
+  date = {
+    day: 0,
+    month: 0,
+    year: 0,
+  }
+
+  ngOnInit(){
+    this.age_form.clearValidators();
+  }
+
+  ageSubmitHandler(){
+    if (this.age_form.invalid) {
+      this.age_form.markAllAsTouched();
+      return;
+    }
+
+    let day = Number(this.age_form.get('day')?.value || 0);
+    let month = Number(this.age_form.get('month')?.value || 0);
+    let year = Number(this.age_form.get('year')?.value || 0);
+
+    console.log(day, month, year);
+
+
+    const today = new Date();
+    const _year = today.getFullYear();
+    const _month = today.getMonth() + 1;
+    const _day = today.getDate();
+
+    console.log(_day, _month, _year);
+
+
+    this.date.year = _year - year - (_month < month || (_month === month && _day < day) ? 1 : 0);
+
+    console.log(this.date);
+
+
+    if (_month < month || (_month === month && _day < day)) {
+      this.date.month = 12 - (month - _month) - (_day < day ? 1 : 0);
+      this.date.day = new Date(_year, month - 1, day).getDate() - _day;
+    } else {
+      this.date.month = _month - month;
+      this.date.day = _day - day;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(),
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
